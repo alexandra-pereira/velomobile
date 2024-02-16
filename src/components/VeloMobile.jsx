3bc7fd6bfc5577286d34ede5ae55e67c2940255a
Alexandra Pereira <xandrapereira@hotmail.com>
2024-02-16 15:24:08 +0100

    carousel fonctione

--- src/components/VeloMobile.jsx
+++ src/components/VeloMobile.jsx
@@ -7,8 +7,8 @@ const VeloMobile = (props) => {
       <h3>{props.veloMobile.model}</h3>
       {isLoggedIn ? (
         <button
-          onClick={()=>{
-            props.handleClickDeleteVeloMobile(props.veloMobile)
+          onClick={() => {
+            props.handleClickDeleteVeloMobile(props.veloMobile);
           }}
           className="btn btn-danger"
         >
@@ -18,10 +18,56 @@ const VeloMobile = (props) => {
       ) : (
         <p> Pas Connecté</p>
       )}
-      <img src={`/images/velosmobiles/${props.veloMobile.photo}`} alt="" />
+      <img
+        src={`/images/velosmobiles/${props.veloMobile.photo}`}
+        alt=""
+        className="velo-mobile-product-image"
+      />
       <p>{props.veloMobile.description}</p>
     </section>
   );
 };
 
 export default VeloMobile;
+
+// import { useOutletContext } from "react-router-dom";
+// import React from "react";
+
+// const VeloMobile = (props) => {
+//   const [isLoggedIn, setIsLoggedIn] = useOutletContext();
+//   return (
+//     <div className="card mb-3">
+//       {" "}
+//       {/* Add margin to the bottom of the card */}
+//       <div className="row no-gutters">
+//         <div className="col-md-4">
+//           <img
+//             src={`/images/velosmobiles/${props.veloMobile.photo}`}
+//             alt={props.veloMobile.model}
+//             className="card-img"
+//           />
+//         </div>
+//         <div className="col-md-8">
+//           <div className="card-body">
+//             <h5 className="card-title">{props.veloMobile.model}</h5>
+//             <p className="card-text">{props.veloMobile.description}</p>
+//             {isLoggedIn ? (
+//               <button
+//                 onClick={() => {
+//                   props.handleClickDeleteVeloMobile(props.veloMobile);
+//                 }}
+//                 className="btn btn-danger"
+//               >
+//                 Supprimer
+//               </button>
+//             ) : (
+//               <p>Pas Connecté</p>
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default VeloMobile;
