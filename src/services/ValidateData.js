bc080789046c35593e3f0d61534c30da8cfde386
anthonybourdeau <tonyr2004@neuf.fr>
2024-02-29 17:32:27 +0100

    mmmm conflit

--- src/services/ValidateData.js
+++ src/services/ValidateData.js
@@ -1,36 +1,18 @@
-<<<<<<< HEAD
+
 export default class ValidateData {
-  static checkChevron(str) {
-    // je dois traiter la chaine (utilisation de la regex)
-    // puis je la renvoie
-
-    return str;
-  }
-
-  static checkIfEmpty(str) {
-    // renvoie un boolean
-   return str === "";
-  }
-}
-=======
-export default class ValidateData{
     static checkChevron(str) {
+        // je dois traiter la chaine (utilisation de la regex)
+        // puis je la renvoie
 
-        return str;  
+        return str;
     }
 
     static checkIfEmpty(str) {
+        // renvoie un boolean
         return str === "";
     }
+}
 
-<<<<<<< HEAD
-   
-}
-=======
-    static dd(params) {
-        console.log(params)
-        alert()
-    }
-}
->>>>>>> e55dc75a0e75931b51956ac821e7cbd01af283ef
->>>>>>> 23266c3973847b96d3068442c3f170eabf4c4cf8
+
+
+
