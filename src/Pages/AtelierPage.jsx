bc23fd9f8f0f07878995449f757a429a8179ad83
Alexandra Pereira <xandrapereira@hotmail.com>
2024-02-29 11:49:22 +0100

    page atelier text + photos workshop

--- src/Pages/AtelierPage.jsx
+++ src/Pages/AtelierPage.jsx
@@ -5,7 +5,15 @@ const AtelierPage = () => {
         <h2 className="card-header mb-4 border-danger rounded-lg text-primary">
           Atelier
         </h2>
-        <p>ici notre atelier</p>
+        <p>Bienvenue dans l'univers artisanal de VéloMobile, où la passion du vélo rencontre l'authenticité du savoir-faire français. Notre atelier, niché au cœur de Mende, est bien plus qu'un simple site de production ; c'est le lieu où naissent nos vélos, conçus avec minutie, passion et un engagement indéfectible envers la qualité artisanale.
+
+            Chez VéloMobile, chaque vélo est façonné à la main par nos artisans talentueux. Chaque courbe, chaque soudure, et chaque détail est méticuleusement travaillé pour créer des vélos uniques qui incarnent l'excellence française. Nous sommes fiers de dire que nos vélos sont non seulement "made in France", mais aussi "fait main", ce qui confère à chaque modèle une touche d'individualité et de soin particulier.
+
+            Au-delà de l'aspect artisanal, nous sommes profondément attachés à nos valeurs écologiques. En choisissant VéloMobile, vous optez pour une démarche responsable envers l'environnement. Nous privilégions des matériaux durables, favorisons les procédés de fabrication respectueux de la planète, et nous efforçons de minimiser notre empreinte carbone à chaque étape de la production.
+
+            Nous comprenons l'importance de la transparence et de la confiance dans le choix d'un vélo. C'est pourquoi nous ouvrons les portes de notre atelier à Mende pour vous inviter à découvrir l'envers du décor. Nos visites guidées vous permettront de plonger au cœur de notre processus de création, d'observer nos artisans à l'œuvre, et de vous immerger dans l'atmosphère unique de VéloMobile.
+
+            En choisissant un vélo VéloMobile, vous faites bien plus qu'acquérir un moyen de transport. Vous adoptez un mode de vie axé sur la qualité, l'authenticité, et la durabilité. Rejoignez-nous à Mende pour une expérience immersive et laissez-vous séduire par l'essence même de VéloMobile. Faites un choix éclairé, choisissez un vélo qui incarne vos valeurs autant que les nôtres.</p>
         </section>
     </>
   );
