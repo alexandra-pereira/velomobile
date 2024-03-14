9d112fd7a80301f2f58201d4aa2d1124eea4e173
anthonybourdeau <tonyr2004@neuf.fr>
2024-03-14 16:35:36 +0100

    push

--- src/components/VeloMobile.jsx
+++ src/components/VeloMobile.jsx
@@ -5,19 +5,16 @@ import Modal from "./Modal";
 
 const VeloMobile = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useOutletContext();
-   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-const openModal = () => {
+  const openModal = () => {
     setIsModalOpen(true);
- };
+  };
 
- const closeModal = () => {
+  const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  
-
-  
   return (
     <div className="card mb-3">
       <div className="row no-gutters">
@@ -43,13 +40,78 @@ const openModal = () => {
                 >
                   Supprimer
                 </button>
-   <div>
-      <button className="btn btn-warning mt-2" onClick={openModal}>Modifier</button>
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-         <FormPutVeloMobile veloMobile={props.veloMobile}/> 
-      </Modal>
+                <div>
+                  <button className="btn btn-warning mt-2" onClick={openModal}>
+                    Modifier
+                  </button>
+                  <Modal isOpen={isModalOpen} onClose={closeModal}>
+                    <div className="card" >
+      <div className="card-body">
+        <form
+          onSubmit={(event) => {
+            handleSubmitFormPostVeloMobile(event);
+          }}
+          action=""
+          className="needs-validation"
+          noValidate
+        >
+          <div className="mb-3">
+            <label htmlFor="model" className="form-label">
+              Modèle
+            </label>
+            <input
+              type="text"
+              id="model"
+              name="model"
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
+            </label>
+            <textarea
+              name="description"
+              id="description"
+              className="form-control"
+              cols="30"
+              rows="3"
+              required
+            ></textarea>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="weight" className="form-label">
+              Poids
+            </label>
+            <input
+              type="text"
+              id="weight"
+              name="poids"
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="photo" className="form-label">
+              Photo
+            </label>
+            <input
+              type="text"
+              id="photo"
+              name="photo"
+              className="form-control"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Envoyer
+          </button>
+        </form>
+      </div>
     </div>
-                 
+                  </Modal>
+                </div>
               </>
             ) : (
               <p>Pas Connecté</p>
